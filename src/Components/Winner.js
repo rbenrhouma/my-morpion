21748ea6cf95bf010f414f7b9d7cd8331c18c7cf
Ridha <rbenrhouma2@gmail.com>
2019-12-25 14:33:43 +0100

    couleur du winner

--- src/Components/Winner.js
+++ src/Components/Winner.js
@@ -4,9 +4,12 @@ import Case from "./Case";
 class Winner extends React.Component {
   render = () => {
     const { winnerName } = this.props;
+    let clName = "";
+    winnerName !== "X" ? (clName = "winnerLabelRed") : (clName = "winnerLabelBlue");
+    console.log(winnerName);
     return (
       <div>
-        <p1 className="winnerLabel">{winnerName != null ? (winnerName === "X" ? "The winner is player 1" : "The winner is player 2") : ""} </p1>
+        <p1 className={clName}>{winnerName != null ? (winnerName === "X" ? "The winner is player 1" : "The winner is player 2") : ""} </p1>
       </div>
     );
   };
