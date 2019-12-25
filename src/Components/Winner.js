40c799b4af0f15dbf478734630930cf17fdaccd2
Ridha <rbenrhouma2@gmail.com>
2019-12-25 15:13:11 +0100

    Ajuoter logo LeReacteur

--- src/Components/Winner.js
+++ src/Components/Winner.js
@@ -1,15 +1,15 @@
 import React from "react";
 import Case from "./Case";
-
+import logo from "./LeReacteur.jpg";
 class Winner extends React.Component {
   render = () => {
     const { winnerName } = this.props;
     let clName = "";
     winnerName !== "X" ? (clName = "winnerLabelRed") : (clName = "winnerLabelBlue");
-    console.log(winnerName);
     return (
       <div>
         <p1 className={clName}>{winnerName != null ? (winnerName === "X" ? "The winner is player 1" : "The winner is player 2") : ""} </p1>
+        <img className="lgoReact" hidden={winnerName === null} src={logo} alt={"logo"} />
       </div>
     );
   };
